cfd480990dd8916e67627f3b5e37371b4bd62b49
DoHoangTrung <63000950+DoHoangTrung@users.noreply.github.com>
2021-05-02 22:03:48 +0700

    upload images

---
+++ Models/DAO/ProductDAO.cs
@@ -0,0 +1,22 @@
+﻿using Hoc_ASP.NET_MVC.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hoc_ASP.NET_MVC.Models.DAO
+{
+    public class ProductDAO
+    {
+        private ShopContext db;
+        public ProductDAO()
+        {
+            db = new ShopContext();
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = db.Products;
+            return products;
+        }
+    }
+}
